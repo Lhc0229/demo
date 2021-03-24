ec8b97f203bd6ee2f310c9c25da643bb77ef0cd4
liumaocong <lmcliumaocong@163.com>
2021-03-24 22:20:52 +0800

    '451'

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = 456
-console.log(123)
-Vue.config.productionTip ='nggggg'
+console.log(456)
+Vue.config.productionTip ='8888'
 
 new Vue({
   render: h => h(App),
