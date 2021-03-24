aa9551aded690551b4edb69572ef6457ef4821cc
liumaocong <lmcliumaocong@163.com>
2021-03-24 22:18:14 +0800

    asd

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = 456
+console.log(123)
+Vue.config.productionTip ='nggggg'
 
 new Vue({
   render: h => h(App),
